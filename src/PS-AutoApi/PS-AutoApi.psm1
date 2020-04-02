2916142077ee77b966c63db14832b97abf8953d4
Amacc <almcchesney@gmail.com>
2020-04-01 19:04:12 -0500

    Adding Function Format-AutoApiResponse

--- src/PS-AutoApi/PS-AutoApi.psm1
+++ src/PS-AutoApi/PS-AutoApi.psm1
@@ -1,1 +1,2 @@
 . $PSScriptRoot\PS-AutoApi.ps1
+. $PSScriptRoot\functions\Format-AutoApiResponse.ps1
---
+++ src/PS-AutoApi/functions/Format-AutoApiResponse.ps1
@@ -0,0 +1,27 @@
+Function Format-AutoApiResponse {
+    [CmdletBinding()]param(
+        $InputObject,
+        [switch] $FormatJson,
+        [switch] $FormatXML,
+        [int]$StatusCode = 200
+    )
+    Write-Verbose "Input $InputObject"
+    $body, $contentType = $(switch ($true) {
+        { $FormatJson } {
+            $( $InputObject | ConvertTo-Json -Compress),
+            "application/json"
+        }
+        { $FormatXML } {
+            $($InputObject | ConvertTo-Xml -As String -NoTypeInformation),
+            "application/xml"
+        }
+        Default { $($InputObject | Out-String), $contentType }
+    })
+    return @{
+        'statusCode' = $StatusCode;
+        'body' = $body
+        'headers' = @{'Content-Type' = $contentType}
+    }
+}
+
+Export-ModuleMember -Function Format-AutoApiResponse
